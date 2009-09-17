6d99772ea519c418aed0232db2f479b345f58466
Josh Borke <joshborke+github@gmail.com>
2009-09-16 22:43:09 -0400

    fix the transactions form

--- transactions/forms.py
+++ transactions/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from redline.transactions.models import Transaction
 
-
 class TransactionForm(forms.ModelForm):
     class Meta:
         model = Transaction
-        fields = ('transaction_type', 'notes', 'category', 'amount', 'date', 'store', 'account', 'hideFromGraph')
+        fields = ('transaction_type', 'notes', 'category', 'amount', 'date', 'account', 'hideFromGraph')
+
