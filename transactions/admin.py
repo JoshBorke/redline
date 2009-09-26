2c2034cc34b9a5195eebb84411c9df6376877861
Josh Borke <joshborke+github@gmail.com>
2009-09-25 23:46:21 -0400

    add the account field to the admin view for transactions

--- transactions/admin.py
+++ transactions/admin.py
@@ -5,14 +5,14 @@ class TransactionAdmin(admin.ModelAdmin):
     date_hierarchy = 'date'
     fieldsets = (
         (None, {
-            'fields': ('transaction_type', 'notes', 'category', 'amount', 'date'),
+            'fields': ('transaction_type', 'notes', 'category', 'amount', 'date', 'account'),
         }),
         ('Metadata', {
             'classes': ('collapse',),
             'fields': ('created', 'updated', 'is_deleted')
         })
     )
-    list_display = ('notes', 'transaction_type', 'amount', 'date', 'is_deleted')
+    list_display = ('notes', 'transaction_type', 'amount', 'date', 'is_deleted', 'account')
     list_filter = ('is_deleted',)
     search_fields = ('notes',)
 
